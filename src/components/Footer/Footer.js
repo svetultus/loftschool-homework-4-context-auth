a40cb1df051f9edd3c3099d7b85d5bbfb3c1c045
svetultus <svetultus@gmail.com>
2019-08-06 16:23:30 +0300

    main functions

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,27 @@
 import React, { PureComponent } from 'react';
 import { AuthConsumer } from '../../contexts/Auth';
 import './Footer.css';
+import SectionTitle from '../SectionTitle';
 
 class Footer extends PureComponent {
   render() {
-    return 'empty';
+    return (
+      <AuthConsumer>
+        {({ isAuthorized, email }) =>
+          isAuthorized ? (
+            <footer className="footer">
+              <SectionTitle className="footer__title">Footer</SectionTitle>
+              <p className="footer-message t-footer">Вы вошли как {email}</p>
+            </footer>
+          ) : (
+            <footer className="footer">
+              <SectionTitle className="footer__title">Footer</SectionTitle>
+              <p className="footer-message t-footer">Вы гость в этой системе</p>
+            </footer>
+          )
+        }
+      </AuthConsumer>
+    );
   }
 }
 
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,28 @@
 import React, { PureComponent } from 'react';
 import './Layout.css';
+import SectionTitle from '../SectionTitle';
 
 class Layout extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.classLayout = 'main';
+
+    if (this.props.header) this.classLayout += ' main--with-header';
+    if (this.props.footer) this.classLayout += ' main--with-footer';
+  }
+
   render() {
-    return 'empty';
+    return (
+      <React.Fragment>
+        {this.props.header}
+        <main className={this.classLayout}>
+          <SectionTitle className="main__title">Main</SectionTitle>
+          {this.props.children}
+        </main>
+        {this.props.footer}
+      </React.Fragment>
+    );
   }
 }
 
